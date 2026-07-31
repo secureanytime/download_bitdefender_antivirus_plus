2203b35cc68d1dd1eae365356b266ad074dde913
secureanytime <jameswoodinfo@gmail.com>
2026-07-31 20:46:57 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,18 +2,30 @@ project = 'Download Bitdefender Antivirus plus'
 copyright = '2026'
 author = 'Admin'
 
-extensions = []
+extensions = [ 'sphinx.ext.autodoc',
+               'sphinx.ext.napoleon',
+               'sphinx_sitemap',
+              ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+html_theme = 'alabaster' # Screenshot wala classic white theme
 
+html_baseurl = 'https://communities-anytime-download-bitdefender-antivirus-plus.readthedocs-hosted.com/en/latest/'
+sitemap_url_scheme = "{link}"
 
+# conf.py
+
+html_title = "Download Bitdefender Antivirus plus"
+html_short_title = "Download Bitdefender Antivirus plus"
+html_static_path = ['_static']
+html_extra_path = ['_static/google5ffeff63dcb91d99.html']
 
 # Meta Tags Configuration
 html_context = {
     'metatags': '''
         <meta name="description" content="Step-by-step guide to the Bitdefender Antivirus Plus download, install, activation, and fixes for common errors. Updated for 2026.">
-        <meta name="[KB3419] Download Bitdefender Antivirus plus" content="docs, guide, setup, tutorial">
+        <meta name="Download Bitdefender Antivirus plus" content="docs, guide, setup, tutorial">
      
     '''
 }
